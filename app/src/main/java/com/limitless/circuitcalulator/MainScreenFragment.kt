e271f7fe54bc33f565d8b22f349491503d92d287
Limitless <theyoungeng@gmail.com>
2022-08-16 01:38:13 +0100

    redesign mainFragment Ui

--- app/src/main/java/com/limitless/circuitcalulator/MainScreenFragment.kt
+++ app/src/main/java/com/limitless/circuitcalulator/MainScreenFragment.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.LinearLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.findNavController
 import com.limitless.circuitcalulator.databinding.MainScreenFragmentBinding
@@ -19,12 +19,12 @@ class MainScreenFragment : Fragment() {
     private lateinit var viewModel: MainScreenViewModel
     private lateinit var viewModelFactory: MainScreenViewModelFactory
     private lateinit var binding: MainScreenFragmentBinding
-    private lateinit var tankCircuit: TextView
-    private lateinit var xformer_params: TextView
-    private lateinit var ohms_law: TextView
-    private lateinit var resistor_color_code: TextView
-    private lateinit var resistor_arrangemnet: TextView
-    private lateinit var network_theoremes: TextView
+    private lateinit var tankCircuit: LinearLayout
+    private lateinit var xformer_params: LinearLayout
+    private lateinit var ohms_law: LinearLayout
+    private lateinit var resistor_color_code: LinearLayout
+    private lateinit var resistor_arrangemnet: LinearLayout
+    private lateinit var network_theoremes: LinearLayout
     private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,28 +45,28 @@ class MainScreenFragment : Fragment() {
         toolbar = binding.mainToolbar
         toolbar.title = getString(R.string.app_name)
 
-        tankCircuit = binding.tankCircuit
+        tankCircuit = binding.rlcViewBtn
         tankCircuit.setOnClickListener { navigateToFragment(tankCircuit) }
 
-        xformer_params = binding.transformerParamBtn
+        xformer_params = binding.xformerViewBtn
         xformer_params.setOnClickListener { navigateToFragment(xformer_params) }
 
-        ohms_law = binding.ohmsLaw
+        ohms_law = binding.ohmsLawViewBtn
         ohms_law.setOnClickListener { navigateToFragment(ohms_law) }
 
-        resistor_color_code = binding.resistorColorCodes
+        resistor_color_code = binding.colorCodeViewBtn
         resistor_color_code.setOnClickListener { navigateToFragment(resistor_color_code) }
 
-        resistor_arrangemnet = binding.equivalentResCap
+        resistor_arrangemnet = binding.resistorArngViewBtn
         resistor_arrangemnet.setOnClickListener { navigateToFragment(resistor_arrangemnet) }
 
-        network_theoremes = binding.networkTheorems
+        network_theoremes = binding.ntwkTheoremsViewBtn
         network_theoremes.setOnClickListener { navigateToFragment(network_theoremes) }
 
         return binding.root
     }
 
-    private fun navigateToFragment(buttonId: TextView) {
+    private fun navigateToFragment(buttonId: LinearLayout) {
         when (buttonId) {
             tankCircuit -> {
                 findNavController().navigate(
